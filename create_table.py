572bcdd4ec84af0451cd779c1017dae8997302a4
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2020-06-11 18:03:23 +0300

    add create table and rewrite methog git of Item

--- create_table.py
+++ create_table.py
@@ -6,5 +6,10 @@ cursor = connection.cursor()
 query = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(query)
 
+query = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cursor.execute(query)
+
+cursor.execute("INSERT INTO items VALUES('test', 10.99)")
+
 connection.commit()
 connection.close()
